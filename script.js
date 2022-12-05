59d82789dc8fcdd8e714b09f15b53bf5f417c5f8
FolorunshoAyo <folushoayomide11@gmail.com>
2022-12-05 16:35:46 +0100

    Made minor changes

--- script.js
+++ script.js
@@ -1,136 +1,167 @@
-// RANDOMIZE GIPHY BAND
+// LAZY LOADING
+document.addEventListener("DOMContentLoaded", function () {
+  // RANDOMIZE GIPHY BAND
 
-// GENERATE RANDOM NUMBER FROM 0 to 255
-function generateRGB() {
-  const rgbValues = [];
+  // GENERATE RANDOM NUMBER FROM 0 to 255
+  function generateRGB() {
+    const rgbValues = [];
 
-  for (let i = 0; i <= 2; i++) {
-    rgbValues.push(Math.floor(Math.random() * 255));
+    for (let i = 0; i <= 2; i++) {
+      rgbValues.push(Math.floor(Math.random() * 255));
+    }
+
+    return rgbValues;
   }
 
-  return rgbValues;
-}
+  function rainbowColorBand() {
+    const bandBoxes = document.querySelectorAll(".box");
 
-function rainbowColorBand() {
-  const bandBoxes = document.querySelectorAll(".box");
+    Array.from(bandBoxes).forEach((bandBox) => {
+      const rgbValues = generateRGB();
+      bandBox.style.backgroundColor = `rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`;
+    });
+  }
 
-  Array.from(bandBoxes).forEach((bandBox) => {
+  function rainbowSpinner() {
+    const spinner = document.querySelector(".spinner");
+
     const rgbValues = generateRGB();
-    bandBox.style.backgroundColor = `rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`;
-  });
-}
-
-function rainbowSpinner() {
-  const spinner = document.querySelector(".spinner");
-
-  const rgbValues = generateRGB();
-  spinner.style.borderTop = `3px solid rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`;
-}
-
-setInterval(() => {
-  rainbowColorBand();
-  rainbowSpinner();
-}, 500);
-
-function randomStr(len, arr) {
-  var ans = "";
-  for (var i = len; i > 0; i--) {
-    ans += arr[Math.floor(Math.random() * arr.length)];
+    spinner.style.borderTop = `3px solid rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`;
   }
-  return ans;
-}
 
-// TRIGGER SEARCH
-const searchBtn = document.getElementById("search-btn");
-const resultsContainer = document.querySelector(".grid.results");
-const preloader = document.querySelector(".grid.preloader");
-let searchResults = [];
-let count = 0;
-let target = 1;
+  setInterval(() => {
+    rainbowColorBand();
+    rainbowSpinner();
+  }, 500);
 
-searchBtn.addEventListener("click", handleQuery);
-// HANDLE QUERY
-function handleQuery() {
-  const searchInput = document.getElementById("query-input");
-  const query = searchInput.value;
+  function randomStr(len, arr) {
+    var ans = "";
+    for (var i = len; i > 0; i--) {
+      ans += arr[Math.floor(Math.random() * arr.length)];
+    }
+    return ans;
+  }
 
-  resultsContainer.innerHTML = "";
-  let html = "";
-  if (!query) {
-    alert("Enter a query!");
-  } else {
-    preloader.classList.add("active");
-    (async () => {
-      const response = await fetch(
-        `https://api.giphy.com/v1/gifs/search?api_key=OfMePKlvNJfEDJsVX2dD7zsRXQMkMjQZ&q=${query}&rating=g&lang=en&random_id=${randomStr(
-          32,
-          "abcde0123456789"
-        )}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
-          },
-        }
-      );
+  // TRIGGER SEARCH
+  const searchBtn = document.getElementById("search-btn");
+  const resultsContainer = document.querySelector(".grid.results");
+  const preloader = document.querySelector(".grid.preloader");
 
-      const data = await response.json();
+  let searchResults = [];
+  let count = 0;
+  let target = 1;
 
-      searchResults = data.data;
+  searchBtn.addEventListener("click", handleQuery);
+  // HANDLE QUERY
+  function handleQuery() {
+    const searchInput = document.getElementById("query-input");
+    const query = searchInput.value;
 
-      preloader.classList.remove("active");
+    resultsContainer.innerHTML = "";
+    let html = "";
+    if (!query) {
+      alert("Enter a query!");
+    } else {
+      preloader.classList.add("active");
+      (async () => {
+        const response = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=OfMePKlvNJfEDJsVX2dD7zsRXQMkMjQZ&q=${query}&rating=g&lang=en&random_id=${randomStr(
+            32,
+            "abcde0123456789"
+          )}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+            },
+          }
+        );
 
-      console.log(searchResults);
-      count = 0;
+        const data = await response.json();
 
-      while (count < 10) {
-        html += `<div class="card">
+        searchResults = data.data;
+
+        preloader.classList.remove("active");
+
+        console.log(searchResults);
+        count = 0;
+
+        while (count < 10) {
+          html += `<div class="card">
                     <div class="image-container">
-                        <img src="${searchResults[count].images.preview_gif.url}" alt="${searchResults[count].title}" />
+                        <img src="${
+                          searchResults[count].images.preview_gif.url
+                        }" alt="${searchResults[count].title}" loading="lazy"/>
                     </div>
-                    <h2 class="display-name">${searchResults[count].user? searchResults[count].user.display_name : ""}</h2>
-                    <p class="caption">${searchResults[count].user? searchResults[count].user.description : ""}</p>
+                    <h2 class="display-name">${
+                      searchResults[count].user
+                        ? searchResults[count].user.display_name
+                        : ""
+                    }</h2>
+                    <p class="caption">${
+                      searchResults[count].user
+                        ? searchResults[count].user.description
+                        : ""
+                    }</p>
                 </div>`;
-        count++;
-      }
-      // Append the first ten GIFs
-      resultsContainer.innerHTML += html;
+          count++;
+        }
+        // Append the first ten GIFs
+        resultsContainer.innerHTML += html;
 
-      window.addEventListener("scroll", () => {
-        if (
-          window.scrollY + window.innerHeight >=
-          document.documentElement.scrollHeight
-        ) {
+        window.addEventListener("scroll", () => {
+          if (
+            window.scrollY + window.innerHeight >=
+            document.documentElement.scrollHeight
+          ) {
             target++;
             loadGIFs();
-        }
-      });
-    })();
+          }
+        });
+      })();
+    }
   }
-}
 
-function loadGIFs() {
-  let html = "";
-  if (count >= 49) {
-    // DO not display anthing
-    document.querySelector(".spinner").classList.remove("active");
-  } else {
-    document.querySelector(".spinner").classList.add("active");
-    setTimeout(() => {
+  function loadGIFs() {
+    let html = "";
+    if (count >= 49) {
+      // DO not display anthing
       document.querySelector(".spinner").classList.remove("active");
-      
-      console.log(`Current count: ${count}, \n Target value is : ${target}`);
-      while (count < (target * 10)) {
-        html += `<div class="card">
+    } else {
+      document.querySelector(".spinner").classList.add("active");
+      setTimeout(() => {
+        document.querySelector(".spinner").classList.remove("active");
+
+        console.log(`Current count: ${count}, \n Target value is : ${target}`);
+        while (count < target * 10) {
+          html += `<div class="card">
                     <div class="image-container">
-                        <img src="${searchResults[count].images.preview_gif.url}" alt="${searchResults[count].title}" />
+                        <img src="${
+                          searchResults[count].images.preview_gif.url
+                        }" loading="lazy" alt="${searchResults[count].title}" />
                     </div>
-                    <h2 class="display-name">${searchResults[count].user? searchResults[count].user.display_name : ""}</h2>
-                    <p class="caption">${searchResults[count].user? searchResults[count].user.description : ""}</p>
+                    <h2 class="display-name">${
+                      searchResults[count].user
+                        ? searchResults[count].user.display_name
+                        : ""
+                    }</h2>
+                    <p class="caption">${
+                      searchResults[count].user
+                        ? searchResults[count].user.description
+                        : ""
+                    }</p>
                 </div>`;
-        count++;
-      }
-      resultsContainer.innerHTML += html;
-    }, 1500);
+          count++;
+        }
+        resultsContainer.innerHTML += html;
+
+        // SCAN FOR NEW IMAGES
+        const lzy_images = document.querySelectorAll("img.lzy_img");
+        lzy_images.forEach((lzy_image) => {
+          // OBSERVE EACH NEWLY RENDERED IMAGE
+          imageObserver.observe(lzy_image);
+        });
+      }, 1500);
+    }
   }
-}
+});
